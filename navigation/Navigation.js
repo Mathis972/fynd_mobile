e5cb4c12b2a1cfad6fe46d71917beea1ca365e6e
EyZex <eyzex@hotmail.fr>
2021-05-09 14:33:50 +0200

    ajout drawer navigation pas trop deg

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -1,6 +1,7 @@
 import LoginScreen from '../screens/LoginScreen';
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { inject, observer } from 'mobx-react';
 
 import CRUDUsers from '../screens/CRUDUsers';
@@ -8,24 +9,37 @@ import { TouchableOpacity, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import CreateUser from '../screens/CRUDUsers/CreateUser';
 import EditUser from '../screens/CRUDUsers/EditUser';
+import ReadQuestions from '../screens/CRUDQuestions/ReadQuestions';
 
 
 
-
+const Drawer = createDrawerNavigator()
 const Stack = createStackNavigator();
 
 const NavigationHandler = (props) => {
     const { loginStore } = props
     return (
-        <Stack.Navigator initialRouteName="LoginScreen">
+        <Drawer.Navigator initialRouteName="LoginScreen" screenOptions={({ route, navigation }) => ({
+            headerRight: () => {
+                if (route.name === "CRUDUsers") {
+                    return <Ionicons style={{ marginRight: 15 }} onPress={() => navigation.navigate('CreateUser')} name="add" size={25} color={"blue"}></Ionicons>
+                } else if (route.name === "CRUDQuestions") {
+                    return <Ionicons style={{ marginRight: 15 }} onPress={() => navigation.navigate('CreateQuestion')} name="add" size={25} color={"blue"}></Ionicons>
+
+                }
+
+            },
+            headerShown: true
+        })}>
             {loginStore.user.isSignedIn
                 ? (
-                    <Stack.Screen name="CRUDUsers" component={UserNavigation}></Stack.Screen>)
-                : (<Stack.Screen name="LoginScreen"
+                    <Drawer.Screen name="CRUDUsers" component={UserNavigation}></Drawer.Screen>)
+                : (<Drawer.Screen name="LoginScreen"
                     component={LoginScreen}
                     options={{ title: 'Login', headerShown: false }}
                 />)}
-        </Stack.Navigator>
+            <Drawer.Screen name="CRUDQuestions" component={QuestionNavigation}></Drawer.Screen>
+        </Drawer.Navigator>
     )
 }
 
@@ -35,11 +49,25 @@ const UserNavigation = () => {
             headerRight: () => {
                 return route.name === "ReadUsers" ? <Ionicons style={{ marginRight: 15 }} onPress={() => navigation.navigate('CreateUser')} name="add" size={25} color={"blue"}></Ionicons> : null
 
-            }
+            },
+            headerShown: false
         })}>
             <Stack.Screen name="ReadUsers" component={CRUDUsers}></Stack.Screen>
             <Stack.Screen name="CreateUser" component={CreateUser}></Stack.Screen>
             <Stack.Screen name="EditUser" component={EditUser}></Stack.Screen>
+        </Stack.Navigator>
+    )
+}
+
+const QuestionNavigation = () => {
+    return (
+        <Stack.Navigator initialRouteName="ReadQuestions" screenOptions={({ route, navigation }) => ({
+            headerRight: () => {
+                // return route.name === "ReadQuestions" ? <Ionicons style={{ marginRight: 15 }} onPress={() => navigation.navigate('CreateUser')} name="add" size={25} color={"blue"}></Ionicons> : null
+
+            }
+        })}>
+            <Stack.Screen name="ReadQuestions" component={ReadQuestions}></Stack.Screen>
         </Stack.Navigator>
     )
 }
---
+++ screens/CRUDQuestions/ReadQuestions.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+
+function ReadQuestions({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Hey</Text>
+        </View>
+    );
+}
+export default ReadQuestions
